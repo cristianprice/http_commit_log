d06ebdd14cd0553e48ef01219117ae573ea9d558
cristianp <cristianp@ceragon.com>
2020-02-26 15:21:51 +0200

    Removed some unused code.

--- config.go
+++ config.go
@@ -9,17 +9,6 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-type Color string
-
-const (
-	ColorBlack  Color = "\u001b[30m"
-	ColorRed          = "\u001b[31m"
-	ColorGreen        = "\u001b[32m"
-	ColorYellow       = "\u001b[33m"
-	ColorBlue         = "\u001b[34m"
-	ColorReset        = "\u001b[0m"
-)
-
 func init() {
 	log.SetFormatter(&log.TextFormatter{
 		FullTimestamp: true,
